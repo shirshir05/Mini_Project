2ee745e0a3a1920ff6fdd19b56cb4e91a10db95b
Shir Cohen <shir.cohen1@motorolasolutions.com>
2020-04-05 10:59:21 +0300

    change logic

--- src/Service_Layer/LogAndExitController.java
+++ src/Service_Layer/LogAndExitController.java
@@ -3,22 +3,13 @@ package Service_Layer;
 public class LogAndExitController extends LogicManagement{
 
 
-    public String Registration(String arg_user_name, String arg_password, String role){
-        if(arg_user_name == null || arg_password == null || arg_user_name.equals("") || arg_password.equals("")){
-            return "The input is empty.";
+    public String Registration(String arg_user_name, String arg_password, String arg_role){
+        String check_input = Input_test(arg_user_name,arg_password);
+        if( check_input!= null){
+            return check_input;
         }
-        if(arg_password.length() != 5){
-            return "The password must contain exactly 5 characters.";
-        }
-        try {
-            int password = Integer.parseInt(arg_password);
-            if (contain_subscription(arg_user_name)){
-                return "Please select another username because this username exists in the system.";
-            }
-
-
-        }catch (Exception e){
-            return "The password must contain only digits.";
+        if (return_enum(arg_role) == -1){
+            return "The role does not exist in the system.";
         }
 
         return "Subscription successfully added!";
@@ -43,4 +34,24 @@ public class LogAndExitController extends LogicManagement{
 
         return true;
     }
+
+    private String Input_test(String arg_user_name, String arg_password){
+        if(arg_user_name == null || arg_password == null || arg_user_name.equals("") || arg_password.equals("")){
+            return "The input is empty.";
+        }
+        if(arg_password.length() != 5){
+            return "The password must contain exactly 5 characters.";
+        }
+        try {
+            int password = Integer.parseInt(arg_password);
+            if (contain_subscription(arg_user_name) != null){
+                return "Please select another username because this username exists in the system.";
+            }
+        }catch (Exception e){
+            return "The password must contain only digits.";
+        }
+        return null;
+    }
+
+
 }
